affb288fbf4cf5307d3cd2216defd4d45f20f964
SirQuackzz <167267691+SirQuackzz@users.noreply.github.com>
2024-04-16 20:47:18 -0500

    Add files via upload

---
+++ game.lua
@@ -0,0 +1,13 @@
+---American Roulette in CC:Tweaked---
+---By Miles Crawford---
+
+local screen = "left"
+local drive = "bottom"
+local version = "0.1"
+local background = colors.green
+term.redirect(peripheral.wrap(screen))
+term.clear()
+
+term.write("TEST")
+
+    
